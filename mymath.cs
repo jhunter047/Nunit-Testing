70b3835b4fa861b3f78fba6a01675574ce2ac21b
joe hunter <jhunter047@gmail.com>
2018-05-31 12:57:24 +0100

    Testing NunitTest

---
+++ mymath.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testdemo
+{
+    class mymath
+    {
+        public int add(int a,int b)
+        {
+            return a + b*3; 
+        }
+
+        public int sub(int a, int b)
+        {
+            return a - b; 
+        }
+    }
+}
---
+++ mytestcase.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testdemo
+{
+    [TestFixture]
+    class mytestcase
+    {
+        [TestCase]
+        public void add()
+        {
+            mymath math = new mymath();
+            Assert.AreEqual(31, math.add(20, 11));
+
+        }
+        [TestCase]
+        public void sub()
+        {
+            mymath math = new mymath();
+            Assert.AreEqual(10, math.sub(20, 10));
+        }
+    }
+}
